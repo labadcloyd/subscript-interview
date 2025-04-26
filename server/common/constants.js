bbf1988260531ceb289168b3a9a4fc884474dbdc
Cloyd Abad <abadcloyd@gmail.com>
2025-04-26 12:09:06 +0800

    updated constants for BASE API url

--- server/common/constants.js
+++ server/common/constants.js
@@ -1,5 +1,5 @@
-const BASE_API = "api/";
-const BASE_API_AUTH = "api/auth/";
-const BASE_API_TODOS = "api/todos/";
+const BASE_API = "/api";
+const BASE_API_AUTH = "/api/auth";
+const BASE_API_TODOS = "/api/todos";
 
 module.exports = { BASE_API, BASE_API_AUTH, BASE_API_TODOS };
